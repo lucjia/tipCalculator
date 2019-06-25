31b9a889faeec7b80c42283e9a53d83ada85e2b9
Lucy Jiang <lucjia@fb.com>
2019-06-25 13:53:15 -0700

    Initial Commit

---
+++ Tip Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tip Calculator
+//
+//  Created by lucjia on 6/25/19.
+//  Copyright © 2019 lucjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
